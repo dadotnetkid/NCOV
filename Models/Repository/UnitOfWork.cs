2bf6265e05a23910cfb6f22cb115018fb3f665bd
Mark Christopher Cacal <markchristopher.cacal@outlook.com>
2020-03-17 10:24:50 +0800

    Add project files.

---
+++ Models/Repository/UnitOfWork.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Models.Repository
+{
+    public partial class UnitOfWork : IDisposable
+    {
+        private ModelDb context = new ModelDb();
+
+
+
+        private GenericRepository<Logs> _LogsRepo;
+        public GenericRepository<Logs> LogsRepo
+        {
+            get
+            {
+                if (this._LogsRepo == null)
+                    this._LogsRepo = new GenericRepository<Logs>(context);
+                return _LogsRepo;
+            }
+            set { _LogsRepo = value; }
+        }
+        
+     
+        private GenericRepository<Provinces> _ProvincesRepo;
+        public GenericRepository<Provinces> ProvincesRepo
+        {
+            get
+            {
+                if (this._ProvincesRepo == null)
+                    this._ProvincesRepo = new GenericRepository<Provinces>(context);
+                return _ProvincesRepo;
+            }
+            set { _ProvincesRepo = value; }
+        }
+
+        private GenericRepository<Towns> _TownsRepo;
+        public GenericRepository<Towns> TownsRepo
+        {
+            get
+            {
+                if (this._TownsRepo == null)
+                    this._TownsRepo = new GenericRepository<Towns>(context);
+                return _TownsRepo;
+            }
+            set { _TownsRepo = value; }
+        }
+     
+      
+        private GenericRepository<Actions> _ActionsRepo;
+        public GenericRepository<Actions> ActionsRepo
+        {
+            get
+            {
+                if (this._ActionsRepo == null)
+                    this._ActionsRepo = new GenericRepository<Actions>(context);
+                return _ActionsRepo;
+            }
+            set { _ActionsRepo = value; }
+        }
+
+        private GenericRepository<UserRolesInActions> _UserRolesInActionsRepo;
+        public GenericRepository<UserRolesInActions> UserRolesInActionsRepo
+        {
+            get
+            {
+                if (this._UserRolesInActionsRepo == null)
+                    this._UserRolesInActionsRepo = new GenericRepository<UserRolesInActions>(context);
+                return _UserRolesInActionsRepo;
+            }
+            set { _UserRolesInActionsRepo = value; }
+        }
+      
+
+  
+        private GenericRepository<ControllersActions> _ControllersActionsRepo;
+        public GenericRepository<ControllersActions> ControllersActionsRepo
+        {
+            get
+            {
+                if (this._ControllersActionsRepo == null)
+                    this._ControllersActionsRepo = new GenericRepository<ControllersActions>(context);
+                return _ControllersActionsRepo;
+            }
+            set { _ControllersActionsRepo = value; }
+        }
+
+     
+
+
+ 
+
+        private GenericRepository<Users> usersRepo;
+        public GenericRepository<Users> UsersRepo
+        {
+            get
+            {
+                if (this.usersRepo == null)
+                    this.usersRepo = new GenericRepository<Users>(context);
+                return usersRepo;
+            }
+            set { usersRepo = value; }
+        }
+
+        private GenericRepository<UserRoles> userRolesRepo;
+        public GenericRepository<UserRoles> UserRolesRepo
+        {
+            get
+            {
+                if (this.userRolesRepo == null)
+                    this.userRolesRepo = new GenericRepository<UserRoles>(context);
+                return userRolesRepo;
+            }
+            set { userRolesRepo = value; }
+        }
+
+
+        public void Save()
+        {
+            context.SaveChanges();
+
+        }
+
+
+        public async Task<int> SaveAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+       
+    }
+
+}
